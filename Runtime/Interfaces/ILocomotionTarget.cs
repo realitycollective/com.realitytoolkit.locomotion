1dc2fc8dead37b4565ff3d650f169d7b5004e5d3
Dino Fejzagić <dino.f@live.de>
2023-04-14 09:24:57 +0200

    Define ILocomotionTarget and remove reference to camera assembly

---
+++ Runtime/Interfaces/ILocomotionTarget.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Reality Collective. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace RealityToolkit.Locomotion.Interfaces
+{
+    /// <summary>
+    /// A <see cref="ILocomotionTarget"/> is the target object being moved by the
+    /// <see cref="ILocomotionSystem"/>.
+    /// </summary>
+    public interface ILocomotionTarget { }
+}
