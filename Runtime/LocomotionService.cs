6fc85e16697132d7fa1d08a08d314e0b349ca4d3
Dino Fejzagić <dino.f@live.de>
2024-05-07 14:43:58 +0200

    Update usings

--- Runtime/LocomotionService.cs
+++ Runtime/LocomotionService.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) Reality Collective. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using RealityCollective.Definitions.Utilities;
-using RealityCollective.Extensions;
 using RealityCollective.ServiceFramework.Attributes;
 using RealityCollective.ServiceFramework.Definitions.Platforms;
+using RealityCollective.ServiceFramework.Definitions.Utilities;
 using RealityCollective.ServiceFramework.Services;
+using RealityCollective.Utilities.Extensions;
 using RealityToolkit.Input.Interfaces;
 using RealityToolkit.Input.Listeners;
 using RealityToolkit.Locomotion.Movement;
