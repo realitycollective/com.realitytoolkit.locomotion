35f52183f35c624dc0bddb729f71cd8eb88e3491
Dino Fejzagić <dino.f@live.de>
2024-05-15 13:38:45 +0200

    Implement LocomotionConfigurationTrigger

--- Runtime/UX/LocomotionConfigurationTrigger.cs
+++ Runtime/UX/LocomotionConfigurationTrigger.cs
@@ -1,6 +1,12 @@
 ﻿// Copyright (c) Reality Collective. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using RealityCollective.ServiceFramework;
+using RealityCollective.ServiceFramework.Attributes;
+using RealityCollective.ServiceFramework.Definitions.Utilities;
+using RealityCollective.ServiceFramework.Services;
+using RealityToolkit.Locomotion.Movement;
+using RealityToolkit.Locomotion.Teleportation;
 using UnityEngine;
 
 namespace RealityToolkit.Locomotion.UX
@@ -9,6 +15,21 @@ namespace RealityToolkit.Locomotion.UX
     [AddComponentMenu(RealityToolkitRuntimePreferences.Toolkit_AddComponentMenu + "/Locomotion/" + nameof(LocomotionConfigurationTrigger))]
     public class LocomotionConfigurationTrigger : MonoBehaviour
     {
+        [SerializeField, Tooltip("If set, teleportation is enabled in this room.")]
+        private bool locomotionEnabled = true;
+
+        [SerializeField, Tooltip("If set, free movement is enabled in this room.")]
+        private bool movementEnabled = true;
+
+        [SerializeField, Tooltip("If set, teleportation is enabled in this room.")]
+        private bool teleportationEnabled = true;
+
+        [SerializeField, Implements(typeof(IFreeLocomotionProvider), TypeGrouping.ByNamespaceFlat)]
+        private SystemType freeProviderType = null;
+
+        [SerializeField, Implements(typeof(ITeleportLocomotionProvider), TypeGrouping.ByNamespaceFlat)]
+        private SystemType teleportProviderType = null;
+
         /// <summary>
         /// See <see cref="MonoBehaviour"/>.
         /// </summary>
@@ -19,6 +40,37 @@ namespace RealityToolkit.Locomotion.UX
             {
                 collider.isTrigger = true;
                 Debug.LogWarning($"{nameof(LocomotionConfigurationTrigger)} requires the attached {nameof(Collider)} to be a trigger and has auto configured it.", this);
+            }
+        }
+
+        /// <summary>
+        /// See <see cref="MonoBehaviour"/>.
+        /// </summary>
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.TryGetComponent<ILocomotionTarget>(out _))
+            {
+                return;
+            }
+
+            if (!ServiceManager.Instance.TryGetService<ILocomotionService>(out var locomotionService))
+            {
+                Debug.LogError($"{nameof(LocomotionConfigurationTrigger)} reuires the {nameof(ILocomotionService)} to work.");
+                return;
+            }
+
+            locomotionService.LocomotionEnabled = locomotionEnabled;
+            locomotionService.MovementEnabled = movementEnabled;
+            locomotionService.TeleportationEnabled = teleportationEnabled;
+
+            if (freeProviderType != null)
+            {
+                locomotionService.EnableLocomotionProvider(freeProviderType.Type);
+            }
+
+            if (teleportProviderType != null)
+            {
+                locomotionService.EnableLocomotionProvider(teleportProviderType.Type);
             }
         }
     }
